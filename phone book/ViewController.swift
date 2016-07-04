4ad8f412c4d2dd4703b7738888ccc8ed7fe6dfdd
13604786021 <CIEC_iMac@ciec-imacs-imac.local>
2016-07-04 15:24:04 +0800

    Initial Commit

---
+++ phone book/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  phone book
+//
+//  Created by CIEC_iMac on 16/7/4.
+//  Copyright © 2016年 CIEC_iMac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
